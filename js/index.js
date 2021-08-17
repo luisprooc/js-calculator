e4b689830f520e60a75fe72bdd140977d254c921
Luis Rosario <luisprooc@gmail.com>
2021-08-17 12:19:09 -0400

    [ADD] Bug fixes

--- js/index.js
+++ js/index.js
@@ -2,16 +2,15 @@ const display = document.getElementById("display").firstElementChild;
 const operators = Array.from(document.querySelectorAll("#operators button"));
 const digits = Array.from(document.querySelectorAll("#digits button"));
 const utils = Array.from(document.querySelectorAll("#utils"));
+let prevValue = "0";
 
 window.addEventListener("load",()=>{
-    //displayAnimation();
     utils.map(util => util.addEventListener("click",(e)=>{
         e.target.id === "clear"? clearFn():EqualFn();   
     }));
 
     operators.map(operator => operator.addEventListener("click",introduceOperator));
     digits.map(digit => digit.addEventListener("click", introduceValue));
-    localStorage.setItem("input","0");
 });
 
 
@@ -28,19 +27,23 @@ function displayAnimation(){
 
 function setDisplay(value){
     display.innerText += value;
-    localStorage.setItem("input",display.innerText);
+    prevValue = value;
 }
 
 function introduceValue(e){
-    if(localStorage.getItem("input")[0] === "0"){
-        display.innerText = "";
-    }
+    if(prevValue === "0") display.innerText = "";
     setDisplay(e.target.innerText);
 }
 
 function introduceOperator(e){
-    if(!/_{3}/.test(display.innerText)){
-        display.innerText += e.target.innerText;
+    if(/[*+/-]/.test(display.innerText) && e.target.innerText !== "-"){
+        display.innerText = display.innerText.slice(0,-1) + e.target.innerText;
+        prevValue = e.target.innerText;
+        return;
+    }
+
+    else if(/[1-9]/.test(prevValue) || e.target.innerText === "-"){
+        setDisplay(e.target.innerText);
     }
 }
 
@@ -48,13 +51,13 @@ function introduceOperator(e){
 function clearFn(){
     if(display.innerText !== "0"){
         display.innerText = "0";
-        localStorage.setItem("input","0");
+        prevValue = "0";
     }
     return;
 }
 
 function EqualFn(){
-    if(/[*+-/]/.test(JSON.stringify(localStorage.getItem("input")))){
-        console.log("YES")
+    if(/[*+-/]/.test(display.innerText) && /\d/.test(prevValue)){
+        console.log("YES");
     }
 }
